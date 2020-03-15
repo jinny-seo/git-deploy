8145f759bfd4addb828fda2dba9b62351b9c257e
Jinny Seo <jinnyseo@p202s-MacBook-Pro-97.local>
2020-03-15 15:31:51 -0500

    Updates

---
+++ precache-manifest.f537af0693283a87720b03e125d8c0a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce990881e6fba7b52e8ed9cc65a359c",
+    "url": "/git-deploy/index.html"
+  },
+  {
+    "revision": "d47dd6d9fdb5982a957d",
+    "url": "/git-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c77e01a1ed779433d5f0",
+    "url": "/git-deploy/static/js/2.fc9535b9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/git-deploy/static/js/2.fc9535b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d47dd6d9fdb5982a957d",
+    "url": "/git-deploy/static/js/main.f72b48e8.chunk.js"
+  },
+  {
+    "revision": "c2b9e3b8adf99de59529",
+    "url": "/git-deploy/static/js/runtime-main.383d7d13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
